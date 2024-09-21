c1e6f668ed7f7dcb9d42fa027bad90deed9eea4b
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-09-21 16:57:38 +0200

    add playwright

--- tests/tests-color/color.spec.js
+++ tests/tests-color/color.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('testColorMode', async ({ page }) => {
-  await page.goto('http://localhost:1313/en/');
+  await page.goto('/');
   await page.getByRole('button', { name: 'theme mode dropdown-toogle' }).click();
   var mode = await page.evaluate(() => document.documentElement.getAttribute('data-bs-theme'));
   await page.getByRole('link', { name: 'Dark' }).click();
