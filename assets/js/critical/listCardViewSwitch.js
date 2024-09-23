696c4e4ace9a4916ea4258837cc62fc9099331b2
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-09-23 11:12:22 +0200

    fix

--- assets/js/critical/listCardViewSwitch.js
+++ assets/js/critical/listCardViewSwitch.js
@@ -1,18 +1,28 @@
+// console.log('{{ .Page.RelPermalink   }}');
+// console.log('{{ .Page.Permalink   }}');
+
+{{ if in .Permalink "/about/" }}
+
 (() => {
   'use strict'
-
+  
   window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('toggleView').addEventListener('click', function() {
-      var listView = document.getElementById('listView');
-      var cardView = document.getElementById('cardView');
-      if (listView.classList.contains('d-none')) {
-        listView.classList.remove('d-none');
-        cardView.classList.add('d-none');
-      } else {
-        listView.classList.add('d-none');
-        cardView.classList.remove('d-none');
-      }
-    });
+    var toogleView = document.getElementById('toggleView');
+    if(toogleView != null) {
+      toogleView.addEventListener('click', function() {
+        var listView = document.getElementById('listView');
+        var cardView = document.getElementById('cardView');
+        if (listView.classList.contains('d-none')) {
+          listView.classList.remove('d-none');
+          cardView.classList.add('d-none');
+        } else {
+          listView.classList.add('d-none');
+          cardView.classList.remove('d-none');
+        }
+      });
+    }
   })
 
 })()  
+
+{{ end -}}
