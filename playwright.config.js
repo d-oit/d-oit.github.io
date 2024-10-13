fdbd4719ba8f140956e929be785530e9b4a74211
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-10-13 20:03:54 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -76,7 +76,7 @@ module.exports = defineConfig({
      ignoreHTTPSErrors: true,
      command: 'npm run start',
      url: baseURL,
-     reuseExistingServer: !process.env.CI,   
+     reuseExistingServer: true,   
   },
   
 });
